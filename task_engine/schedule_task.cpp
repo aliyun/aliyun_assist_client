39e52a1733005e96d47cd08d1a109567e35801f2
feng.changf1 <feng.changf1@alibaba-inc.com>
2017-09-11 19:22:13 +0800

    Fix bug.

--- task_engine/schedule_task.cpp
+++ task_engine/schedule_task.cpp
@@ -48,7 +48,7 @@ void TaskSchedule::TestFetch(std::string info) {
   std::vector<TaskInfo> tasks;
   task_engine::TaskFetch task_fetch;
   Encoder encoder;
-  char* pencodedata = encoder.B64Encode(
+  char* pencodedata = encoder.B64Decode(
       (const unsigned char *)info.c_str(), info.size());
   task_fetch.TestFetchTasks(pencodedata, tasks);
 
