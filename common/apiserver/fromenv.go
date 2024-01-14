3d55c456973c01e753ed5e189d9c847eb4d428e7
LXM <lxm.xupt@gmail.com>
2024-01-14 15:24:14 +0800

    fix: provider fromenv missing extraheaders

--- common/apiserver/fromenv.go
+++ common/apiserver/fromenv.go
@@ -4,13 +4,19 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 
+	"github.com/aliyun/aliyun_assist_client/agent/util/osutil"
+	"github.com/aliyun/aliyun_assist_client/agent/util/timetool"
 	"github.com/aliyun/aliyun_assist_client/thirdparty/sirupsen/logrus"
+	"github.com/google/uuid"
 
+	"github.com/aliyun/aliyun_assist_client/common/machineid"
+	"github.com/aliyun/aliyun_assist_client/common/pathutil"
 	"github.com/aliyun/aliyun_assist_client/common/requester"
 )
 
-type EnvironmentVariableProvider struct {}
+type EnvironmentVariableProvider struct{}
 
 func (*EnvironmentVariableProvider) Name() string {
 	return "EnvironmentVariableProvider"
@@ -45,5 +51,42 @@ func (*EnvironmentVariableProvider) ServerDomain(logger logrus.FieldLogger) (str
 }
 
 func (*EnvironmentVariableProvider) ExtraHTTPHeaders(logger logrus.FieldLogger) (map[string]string, error) {
-	return nil, nil
+	domain := os.Getenv("ALIYUN_ASSIST_SERVER_HOST")
+	if domain == "" {
+		return nil, requester.ErrNotProvided
+	}
+
+	u4 := uuid.New()
+	str_request_id := u4.String()
+
+	timestamp := timetool.GetAccurateTime()
+	str_timestamp := strconv.FormatInt(timestamp, 10)
+
+	var instance_id string
+	var path string
+	path, _ = pathutil.GetHybridPath()
+
+	content, _ := os.ReadFile(path + "/instance-id")
+	instance_id = string(content)
+
+	mid, _ := machineid.GetMachineID()
+
+	input := instance_id + mid + str_timestamp + str_request_id
+	pri_key, _ := os.ReadFile(path + "/pri-key")
+	output := rsaSign(logger, input, string(pri_key))
+	logger.Infoln(input, output)
+
+	extraHeaders := map[string]string{
+		"x-acs-instance-id": instance_id,
+		"x-acs-timestamp":   str_timestamp,
+		"x-acs-request-id":  str_request_id,
+		"x-acs-signature":   output,
+	}
+
+	internal_ip, err := osutil.ExternalIP()
+	if err == nil {
+		extraHeaders["X-Client-IP"] = internal_ip.String()
+	}
+
+	return extraHeaders, nil
 }
